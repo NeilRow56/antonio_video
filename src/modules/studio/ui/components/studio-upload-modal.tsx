5938e1a2cd85fc64d11e21c7d22a17ed00f9d7fb
NeilRow56 <neilrowland56@gmail.com>
2025-05-06 12:07:46 +0100

    Added a responsive-dialog component

--- src/modules/studio/ui/components/studio-upload-modal.tsx
+++ src/modules/studio/ui/components/studio-upload-modal.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import { ResponsiveModal } from '@/components/responsive-dialog'
 import { Button } from '@/components/ui/button'
 import { trpc } from '@/trpc/client'
 import { Loader2Icon, PlusIcon } from 'lucide-react'
@@ -17,18 +18,28 @@ export const StudioUploadModal = () => {
     }
   })
   return (
-    <Button
-      className='cursor-pointer'
-      disabled={create.isPending}
-      variant='secondary'
-      onClick={() => create.mutate()}
-    >
-      {create.isPending ? (
-        <Loader2Icon className='animate-spin' />
-      ) : (
-        <PlusIcon size='8' />
-      )}
-      Create
-    </Button>
+    <>
+      <ResponsiveModal
+        title='Upload a video'
+        // !! coercion to a boolean value. Means that you want to make sure your resulting value is either true or false, not undefined or [ ].
+        open={!!create.data}
+        onOpenChange={() => create.reset()}
+      >
+        <p>This will be an uploader</p>
+      </ResponsiveModal>
+      <Button
+        className='cursor-pointer'
+        disabled={create.isPending}
+        variant='secondary'
+        onClick={() => create.mutate()}
+      >
+        {create.isPending ? (
+          <Loader2Icon className='animate-spin' />
+        ) : (
+          <PlusIcon size='8' />
+        )}
+        Create
+      </Button>
+    </>
   )
 }
