2a8b7342791efc93aef606de83a36ba81c977f6b
NeilRow56 <neilrowland56@gmail.com>
2025-05-07 07:39:59 +0100

    Added mux uploader to studio

--- src/modules/studio/ui/components/studio-upload-modal.tsx
+++ src/modules/studio/ui/components/studio-upload-modal.tsx
@@ -5,6 +5,7 @@ import { Button } from '@/components/ui/button'
 import { trpc } from '@/trpc/client'
 import { Loader2Icon, PlusIcon } from 'lucide-react'
 import { toast } from 'sonner'
+import { StudioUploader } from './studio-uploader'
 
 export const StudioUploadModal = () => {
   const utils = trpc.useUtils()
@@ -22,10 +23,14 @@ export const StudioUploadModal = () => {
       <ResponsiveModal
         title='Upload a video'
         // !! coercion to a boolean value. Means that you want to make sure your resulting value is either true or false, not undefined or [ ].
-        open={!!create.data}
+        open={!!create.data?.url}
         onOpenChange={() => create.reset()}
       >
-        <p>This will be an uploader</p>
+        {create.data?.url ? (
+          <StudioUploader endpoint={create.data.url} onSuccess={() => {}} />
+        ) : (
+          <Loader2Icon className='animate-spin' />
+        )}
       </ResponsiveModal>
       <Button
         className='cursor-pointer'
